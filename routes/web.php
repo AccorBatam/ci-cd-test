48b6b6c9b8497753950c02e23fa1b57e535506b6
Pirman Abdurohman <pirman.abdurohman@id.mediact.com>
2021-01-24 16:59:28 +0700

    Add Task for deployer configuration

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 use App\Http\Controllers\SampleController;
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'SampleController@index');
 Route::get('/test', 'SampleController@test');
